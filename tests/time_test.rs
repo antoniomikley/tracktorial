4c17fecb681a976b15310d889dd5d975b5889849
Antonio Mikley <antonio.mikley@gmail.com>
2024-06-05 17:34:22 +0200

    fix test

--- tests/time_test.rs
+++ tests/time_test.rs
@@ -1,7 +1,9 @@
+use chrono::Timelike;
+
 #[test]
 fn time_parse_as_hms() {
     let parsed_time = tracktorial::time::parse_date_time("18:30:45").unwrap();
-    let naive_time = chrono::NaiveTime::from_hms_opt(18, 30, 48).unwrap();
+    let naive_time = chrono::NaiveTime::from_hms_opt(18, 30, 45).unwrap();
     assert_eq!(
         chrono::Local::now().with_time(naive_time).unwrap(),
         parsed_time
