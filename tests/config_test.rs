41be5dd4b25ee22ba3feac2f4567e7ce1b64122a
Antonio Mikley <antonio.mikley@gmail.com>
2024-06-06 11:35:59 +0200

    add test

--- tests/config_test.rs
+++ tests/config_test.rs
@@ -1,7 +1,17 @@
-use tracktorial::config::Configuration;
+use tracktorial::{api::FactorialApi, config::Configuration, login::Credential};
 
 #[test]
 fn can_retrieve_config_file() {
     let config = Configuration::get_config();
     assert_eq!(true, config.is_ok());
+}
+
+#[test]
+fn config_with_only_email_gets_repopulated_on_login() {
+    let mut minimal_config = Configuration::default();
+    let my_config = Configuration::get_config().unwrap();
+    minimal_config.email = my_config.email.clone();
+    let cred = Credential::new_without_password(&minimal_config.email);
+    FactorialApi::new(cred, &mut minimal_config).unwrap();
+    assert_eq!(my_config, minimal_config);
 }
